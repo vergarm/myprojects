2f0b5c011a777a25adefe42e1fd31afc488b57de
vergarm <mvvergar@outlook.com>
2017-02-25 23:23:41 -0500

    unit test

---
+++ UnitTestWebApp/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApp.Controllers;
+using System.Web.Mvc;
+
+namespace UnitTestWebApp
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestDetailsView()
+        {
+            var controller = new HomeController();
+            var result = controller.Details(2) as ViewResult;
+            Assert.AreEqual("Details", result.ViewName);
+
+        }
+    }
+}
